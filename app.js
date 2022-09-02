e8c12051cb36c84c08c4d105d4551a0fffb65941
Deepak kumar <dk815617@gmail.com>
2022-09-02 08:39:27 +0530

    cors

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const User = require("./models/users");
 const bodyParser = require("body-parser")
 const app = express();
+const cors = require("cors")
 app.use(express.json())
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(require('./router/route'));
